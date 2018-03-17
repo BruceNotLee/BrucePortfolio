cc45705fb482a09f6f37ec447234bc2b45bdd7c0
BruceNotLee <bruce.lesko@outlook.com>
2018-03-17 19:23:34 -0400

    added seed file

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+9.times do |x|
+ Blog.create!(
+  title: "#{Faker::SiliconValley.company}",
+  body: "#{Faker::SiliconValley.motto}"
+  )
+end
+
+6.times do |x|
+  Portfolio.create!(
+  title: "#{Faker::SiliconValley.app}",
+  subtitle: "#{Faker::SiliconValley.invention}",
+  body: "#{Faker::SiliconValley.quote}",
+  main_image: "http://via.placeholder.com/600x600",
+  thumb_image: "http://via.placeholder.com/300x300"
+  )
+end
+
+10.times do |x|
+  Skill.create!(
+    title: "#{Faker::ProgrammingLanguage.name}",
+    percent_utilized: 100
+  )
+end
