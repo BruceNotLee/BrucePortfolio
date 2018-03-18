a36fbce430b84096b4410f0a0cbf00fc648ee0fd
BruceNotLee <bruce.lesko@outlook.com>
2018-03-18 16:22:05 -0400

    added validations to blogs portfolio and skill

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,5 @@ class Portfolio < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
+  validates_presence_of :title, :body, :thumb_image, :main_image
 end
