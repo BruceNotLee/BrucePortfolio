d1dd4b22b0d132f6d599be4da4243eefe1f4a200
BruceNotLee <bruce.lesko@outlook.com>
2018-03-18 13:43:44 -0400

    published status added

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   get 'privacy', to: 'pages#privacy'
 
-  resources :blogs
-  
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 end
