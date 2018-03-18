147c8b2922b9175e15a1089342b0ddc4bf63cd25
BruceNotLee <bruce.lesko@outlook.com>
2018-03-17 21:53:18 -0400

    custom portfolio show

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get 'privacy', to: 'pages#privacy'
 
   resources :blogs
-  resources :portfolios
+  
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 end
