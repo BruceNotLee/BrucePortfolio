a33974fa4c12348b6ef548c3ebdfb0c2fba6415c
BruceNotLee <bruce.lesko@outlook.com>
2018-03-18 17:13:11 -0400

    set default images for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,5 +10,10 @@ class Portfolio < ApplicationRecord
   scope :html, -> { where(subtitle: 'HTML') }
   scope :css, -> { where(subtitle: 'CSS') }
 
+  after_initialize :set_defaults
 
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x600"
+    self.thumb_image ||= "http://via.placeholder.com/300x300"
+  end
 end
