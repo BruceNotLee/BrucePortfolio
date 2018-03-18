4e9643656164ba1564eab5c52d56d536356840af
BruceNotLee <bruce.lesko@outlook.com>
2018-03-17 21:47:26 -0400

    routes customized

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/privacy'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'privacy', to: 'pages#privacy'
 
   resources :blogs
   resources :portfolios
