d63a8921cecf48e6360baa3902c57b351be7d30e
BruceNotLee <bruce.lesko@outlook.com>
2018-03-18 14:20:38 -0400

    updated routes to remove extra resource

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,8 @@ Rails.application.routes.draw do
 
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  resources :portfolios do
-    member do
-      get :toggle_status
-    end
-  end
+  
+   get 'portfolios/:id', to: 'portfolios#toggle_status', as: 'toggle_status_portfolio' do
+     get :toggle_status
+   end
 end
